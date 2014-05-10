ca4ecd820d20615451459bb1fb78f39cdb7ce0bf
Kevin Zhou <KevZho141@gmail.com>
2014-05-09 21:55:38 -0700

    Update show.php

--- server/show.php
+++ server/show.php
@@ -1,5 +1,5 @@
 <?php
-    include_once "vars.php";
+	include_once "vars.php";
 	$result = mysql_query("SELECT * FROM projects") or die("Query failed");
 	$projects = array();
 	while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
